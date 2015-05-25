7de4b673de6a2eb4743e312010b7dcb36fe17759
Mike Neville <mneville3@gmail.com>
2015-05-24 23:10:47 -0700

    Finished 215e

---
+++ 215e/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _215e
+{
+    class Program
+    {
+        // See http://www.reddit.com/r/dailyprogrammer/comments/36cyxf/20150518_challenge_215_easy_sad_cycles/
+
+        static void Main(string[] args)
+        {
+            int b = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, int> sad = new Dictionary<string, int>();
+
+            int sum = n;
+
+            while (true)
+            {
+                sum = SumPoweredDigits(b, sum);
+                string s = sum.ToString();
+
+                if (sad.ContainsKey(s))
+                {
+                    if (sad[s] >= 1)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        sad[s]++;
+                    }
+                }
+                else
+                {
+                    sad.Add(s, 0);
+                }
+            }
+
+            var cycle = from c in sad
+                        where c.Value > 0
+                        select c.Key;
+
+            Console.WriteLine(string.Join(", ", cycle));
+            Console.ReadLine();
+
+        }
+
+        static int SumPoweredDigits(int power, int number)
+        {
+            int sum = 0;
+
+            while (number > 0)
+            {
+                int temp = number % 10;
+                sum += (int) Math.Pow(temp, power);
+                number /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
